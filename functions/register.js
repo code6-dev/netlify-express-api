6d30239b09212647dd48c19b33cdc2f6eb78715c
Brett Alessandri <balessandri1@outlook.com>
2021-08-16 18:30:48 -0600

    debug

--- functions/register.js
+++ functions/register.js
@@ -7,18 +7,6 @@ const User = require('../models/User');
 exports.handler = async function (event, context) {
   const body = JSON.parse(event.body);
 
-  // Set DB connection
-  mongoose.connect(
-    process.env.DB_HOST,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
-    },
-    () => console.log('DB Connected')
-  );
-
   //  Validate data
   const { error, value } = validateRegistration(body);
   if (error) {
@@ -27,29 +15,45 @@ exports.handler = async function (event, context) {
       body: JSON.stringify({ message: 'Invalid data\n' + error.details.map((m) => m.message) })
     };
   }
+  var newUser;
 
   //  Hash Password
   const salt = await bcrypt.genSalt(10);
   const hashed = await bcrypt.hash(value.password, salt);
+  console.log(hashed ? 'Good Hash' : 'No Hash');
 
-  //  Process user details and save to DB
-  const newUser = new User({
-    name: value.name,
-    email: value.email,
-    password: hashed
-  });
+  // Set DB connection
+  mongoose
+    .connect(process.env.DB_HOST, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then((c) => {
+      //  Process user details and save to DB
+      newUser = new User({
+        name: value.name,
+        email: value.email,
+        password: hashed
+      });
+      console.log(newUser);
+      const result = await newUser.save();
 
-  const result = await newUser.save();
-
-  if (!!result) {
-    return {
-      status: 200,
-      body: JSON.stringify({ id: user.id })
-    };
-  } else {
-    return {
-      status: 500,
-      body: JSON.stringify({ error: 'User not saved' })
-    };
-  }
+      if (!!result) {
+        return {
+          status: 200,
+          body: JSON.stringify({ id: user.id })
+        };
+      } else {
+        return {
+          status: 500,
+          body: JSON.stringify({ error: 'User not saved' })
+        };
+      }
+    })
+    .catch((err) => {
+      return {
+        status: 500,
+        body: JSON.stringify({ 'db error': err })
+      };
+    });
 };
