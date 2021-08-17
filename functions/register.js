2c7ab0b4c30fda4051c891d68b7a7c275980ac2b
Brett Alessandri <balessandri1@outlook.com>
2021-08-16 18:37:58 -0600

    refactor db connection

--- functions/register.js
+++ functions/register.js
@@ -6,7 +6,7 @@ const User = require('../models/User');
 
 exports.handler = async function (event, context) {
   const body = JSON.parse(event.body);
-
+  console.log(context);
   //  Validate data
   const { error, value } = validateRegistration(body);
   if (error) {
@@ -15,45 +15,39 @@ exports.handler = async function (event, context) {
       body: JSON.stringify({ message: 'Invalid data\n' + error.details.map((m) => m.message) })
     };
   }
-  var newUser;
 
   //  Hash Password
   const salt = await bcrypt.genSalt(10);
   const hashed = await bcrypt.hash(value.password, salt);
   console.log(hashed ? 'Good Hash' : 'No Hash');
 
-  // Set DB connection
-  mongoose
-    .connect(process.env.DB_HOST, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    .then(async (c) => {
-      //  Process user details and save to DB
-      newUser = new User({
-        name: value.name,
-        email: value.email,
-        password: hashed
-      });
-      console.log(newUser);
-      const result = await newUser.save();
+  const newUser = new User({
+    name: value.name,
+    email: value.email,
+    password: hashed
+  });
 
-      if (!!result) {
-        return {
-          status: 200,
-          body: JSON.stringify({ id: user.id })
-        };
-      } else {
-        return {
-          status: 500,
-          body: JSON.stringify({ error: 'User not saved' })
-        };
-      }
-    })
-    .catch((err) => {
-      return {
-        status: 500,
-        body: JSON.stringify({ 'db error': err })
-      };
-    });
+  // Set DB connection
+  const db = await mongoose.connect(process.env.DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  //  save to DB
+  console.log(newUser);
+  const result = await newUser.save();
+
+  if (!!result) {
+    db.disconnect();
+    return {
+      status: 200,
+      body: JSON.stringify({ id: user.id })
+    };
+  } else {
+    db.disconnect();
+    return {
+      status: 500,
+      body: JSON.stringify({ error: 'User not saved' })
+    };
+  }
 };
